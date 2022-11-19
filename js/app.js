ec4edaa6d4fa6db9f33fad15401a6a72b7ea2128
oniel santiago <onielst84@gmail.com>
2022-11-19 13:14:06 -0300

    validando dados do formulário

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let cliente = {
+  mesa: '',
+  hora: '',
+  pedido: []
+}
+
+const btnSalvarCliente = document.querySelector('#guardar-cliente');
+
+btnSalvarCliente.addEventListener('click', salvarCliente);
+
+function salvarCliente() {
+  const hora = document.querySelector('#hora').value;
+  const mesa = document.querySelector('#mesa').value;
+
+  
+
+  const camposVacios = [ mesa, hora ].some( campo => campo === '');
+
+  if(camposVacios){
+    //verificar se está mostrando-se a alerta
+    const existaAlerta = document.querySelector('.invalid-feedback');
+
+    if(!existaAlerta) {
+      const alerta = document.createElement('div');
+      alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+      alerta.textContent = 'Todos os campos são obrigatórios';
+      document.querySelector('.modal-body form').appendChild(alerta);
+
+      setTimeout(() => {
+        alerta.remove();
+      }, 2500);
+    }
+
+    return;
+
+  }
+  
+  console.log('Campos preechidos corretamente');
+
+    
+}
