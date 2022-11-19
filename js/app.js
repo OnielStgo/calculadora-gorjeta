a22be5f57f87bee0ad0bb971322326206d72ce0f
oniel santiago <onielst84@gmail.com>
2022-11-19 16:53:01 -0300

    obtendo dados da API e mostrando-os no site

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
   pedido: []
 }
 
+const categorias = {
+  1: 'Comida',
+  2: 'Suco',
+  3: 'Sobremesa'
+}
+
 const btnSalvarCliente = document.querySelector('#guardar-cliente');
 
 btnSalvarCliente.addEventListener('click', salvarCliente);
@@ -43,4 +49,65 @@ function salvarCliente() {
   const modalInstancia = bootstrap.Modal.getInstance(modal);
   modalInstancia.hide();
 
+  //mostar seções
+  mostrarSecoes();
+
+  //obter platilhos da API de JSON-SERVER
+  obterPlatilhos();
+
+}
+
+function mostrarSecoes(){
+  const secoes = document.querySelectorAll('.d-none');
+
+  secoes.forEach(secao => secao.classList.remove('d-none'));
+}
+
+function obterPlatilhos() {
+  const url = 'http://localhost:4000/platillos';
+
+  fetch(url)
+    .then(resposta => resposta.json() )
+    .then(resultado => mostrarPlatilhos(resultado))
+    .catch(erro => console.log(erro))
+}
+
+function mostrarPlatilhos(platilhos){
+
+  const conteudo = document.querySelector('#platillos .contenido');
+  
+  platilhos.forEach(platilho => {
+    const row = document.createElement('div');
+    row.classList.add('row', 'py-2', 'border-top');
+
+    const nombre = document.createElement('div');
+    nombre.classList.add('col-md-4')
+    nombre.textContent = platilho.nombre;
+
+    const preco = document.createElement('div');
+    preco.classList.add('col-md-3', 'fw-bold');
+    preco.textContent = `$${platilho.precio}`;
+
+    const categoria = document.createElement('div');
+    categoria.classList.add('col-md-3')
+    categoria.textContent = categorias[platilho.categoria];
+
+    const inputCantidad = document.createElement('input');
+    inputCantidad.classList.add('form-control');
+    inputCantidad.type = 'number';
+    inputCantidad.value = 0;
+    inputCantidad.min = 0;
+    inputCantidad.id = `produto-${platilho.id}`;
+
+    const caixaDoInput = document.createElement('div');
+    caixaDoInput.classList.add('col-md-2');
+    caixaDoInput.appendChild(inputCantidad);
+
+    row.appendChild(nombre);
+    row.appendChild(preco);
+    row.appendChild(categoria);
+    row.appendChild(caixaDoInput);
+
+    conteudo.appendChild(row);
+  })
 }
